2d8523c0d46a8730aa1c6a5ec461d569522dc28b
MavrinV <167757992+MavrinV@users.noreply.github.com>
2025-06-12 13:37:38 +0100

    Update NoteList.tsx

--- src/components/NoteList/NoteList.tsx
+++ src/components/NoteList/NoteList.tsx
@@ -1,28 +1,48 @@
+import {type Note} from "../../types/note";
 import css from "./NoteList.module.css";
-import { useDeleteNote } from "../../hooks/NoteCreateHook/HookMutation";
-import type { NoteProps } from "../../types/note";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { removeNote } from "../../services/noteService";
+import iziToast from "izitoast";
 
-export default function NoteList({ notes }: NoteProps) {
-  const { mutate, isPending } = useDeleteNote();
+interface NoteListProps {
+  notes: Note[];
+}
 
+export default function NoteList({ notes }: NoteListProps) {
+  const queryClient = useQueryClient();
+  const deleteNote = useMutation({
+    mutationFn: (id: number) => removeNote(id),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["Notes"],
+      });
+    },
+    onError: () => {
+      iziToast.error({
+        message: "Error deleting note, please try again",
+        position: "topCenter",
+      });
+    },
+  });
   return (
     <ul className={css.list}>
-      {notes.map((note) => (
-        <li className={css.listItem} key={note.id}>
-          <h2 className={css.title}>{note.title}</h2>
-          <p className={css.content}>{note.content}</p>
-          <div className={css.footer}>
-            <span className={css.tag}>{note.tag}</span>
-            <button
-              className={css.button}
-              onClick={() => mutate(note.id)}
-              disabled={isPending}
-            >
-              Delete
-            </button>
-          </div>
-        </li>
-      ))}
+      {notes.map((note) => {
+        return (
+          <li key={note.id} className={css.listItem}>
+            <h2 className={css.title}>{note.title}</h2>
+            <p className={css.content}>{note.content}</p>
+            <div className={css.footer}>
+              <span className={css.tag}>{note.tag}</span>
+              <button
+                onClick={() => deleteNote.mutate(note.id)}
+                className={css.button}
+              >
+                Delete
+              </button>
+            </div>
+          </li>
+        );
+      })}
     </ul>
   );
 }
